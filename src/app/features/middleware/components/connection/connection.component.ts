7bf2ea85df5072a019da6c4897837b909f82d479
JiaYuin <jiayuin26@gmail.com>
2024-11-07 19:49:59 +0900

    updated connection ts

--- src/app/features/middleware/components/connection/connection.component.ts
+++ src/app/features/middleware/components/connection/connection.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -10,38 +11,114 @@ export class ConnectionComponent {
   keyword: string = '';  // Holds the input keyword
   searchResults: any[] = [];  // Holds the search results
 
-  constructor(private http: HttpClient) {}
+  private _title: string = "";
+  private _currentMenu: string = "";
+  constructor(private router: Router) { }
 
-  onSearch() {
-    console.log('Search initiated with keyword:', this.keyword);
+  ngOnInit(): void {
+    let url = this.router.url.split("/");
+    this.currentMenu = url[url.length - 1];
+    this.setTitle(this.currentMenu);
+  }
 
-    if (!this.keyword.trim()) {
-      alert('Please enter a keyword!');
-      return;
+  selectedStyleObject(flag: boolean): Object {
+    if(matchMedia("(max-width: 425px)").matches) {
+      if (flag) {
+        return {
+          "font-weight": "bold",
+          "border-bottom" : "0.2rem solid #0FBAFF",
+        };
+      } else {
+        return {
+          color: "#898C8D",
+          "background-color": "white",
+        };
+      }
+    }else{
+      if (flag) {
+        return {
+          color: "#0FBAFF",
+          "font-weight": "bold",
+        };
+      } else {
+        return {
+          color: "black",
+          "background-color": "white",
+        };
+      }
     }
+  }
+  /**
+   * @description Set title according to current address
+   * @param currentAddress
+   */
+  setTitle(currentAddress: string) {
+    if (currentAddress === "manual") this.title = 'File Upload';
+    if (currentAddress === "preprocessing") this.title = "Search Menu";
+    if (currentAddress === "analysis") this.title = "Analysis Menu";
+  }
+
+  toFileUpload() {
+    this.router.navigateByUrl("/components/connection/fileupload");
+    this.ngOnInit();
+  }
+
+  toElasticSearch() {
+    this.router.navigateByUrl("/components/connection/search");
+    this.ngOnInit();
+  }
+
+  toAnalysis() {
+    this.router.navigateByUrl("/components/connection/analysis");
+    this.ngOnInit();
+  }
+
+  public get currentMenu(): string {
+    return this._currentMenu;
+  }
+  public set currentMenu(value: string) {
+    this._currentMenu = value;
+  }
+  public get title(): string {
+    return this._title;
+  }
+  public set title(value: string) {
+    this._title = value;
+  }
+
+
+  // constructor(private http: HttpClient) {}
+
+  // onSearch() {
+  //   console.log('Search initiated with keyword:', this.keyword);
+
+  //   if (!this.keyword.trim()) {
+  //     alert('Please enter a keyword!');
+  //     return;
+  //   }
 
     // Update the URL with the full address of your middleware
-    const middlewareUrl = 'http://localhost:10000/esQueryTest';
+    // const middlewareUrl = 'http://localhost:10000/esQueryTest';
 
-    // Send the keyword to the backend
-    this.http.post<any>(middlewareUrl, { keyword: this.keyword }).subscribe(
-      (response) => {
-        console.log('Search response received:', response);
-        if (response && response.results) {
-          this.searchResults = response.results;  // Assume response contains 'results' field
+    // // Send the keyword to the backend
+    // this.http.post<any>(middlewareUrl, { keyword: this.keyword }).subscribe(
+    //   (response) => {
+    //     console.log('Search response received:', response);
+    //     if (response && response.results) {
+    //       this.searchResults = response.results;  // Assume response contains 'results' field
         
-          // this.searchResults.forEach(result => {
-          //   console.log('Document ID:', result._id);
-          //   console.log('Document Data:', result.source);
-          // });
-        } else {
-          this.searchResults = [];  // No results found
-        }
-      },
-      (error) => {
-        console.error('Error:', error);
-        alert('An error occurred while fetching data.');
-      }
-    );
-  }
+    //       // this.searchResults.forEach(result => {
+    //       //   console.log('Document ID:', result._id);
+    //       //   console.log('Document Data:', result.source);
+    //       // });
+    //     } else {
+    //       this.searchResults = [];  // No results found
+    //     }
+    //   },
+    //   (error) => {
+    //     console.error('Error:', error);
+    //     alert('An error occurred while fetching data.');
+    //   }
+    // );
+  // }
 }
